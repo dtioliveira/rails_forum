a81e6169309d6661d6cf79888123ed761fc7be8f
Diego Oliveira <dti.oliveira@live.com>
2015-12-22 20:34:16 -0200

    Adicionando flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,16 @@ Rails.application.routes.draw do
   }
 
   get :home, to: 'pages#home', as: :home
-  root 'pages#home'
+
+  authenticated :user do
+    devise_scope :user do
+      root to: "pages#home", as: "root"
+    end
+  end
+
+  unauthenticated do
+    devise_scope :user do
+      root to: "user/registrations#new", as: "unauthenticated"
+    end
+  end
 end
