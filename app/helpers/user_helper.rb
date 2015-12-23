7d8d08548496342f608829f8c5019c04d214046e
Diego Oliveira <dti.oliveira@live.com>
2015-12-23 20:00:11 -0200

    Alterações no menu lateral

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,4 +3,8 @@ module UserHelper
   def is_admin(user)
     "<div><strong>ADMIN</strong></div>".html_safe if user.is_admin
   end
+
+  def display_admin_cert(user)
+    "<div class='admin-cert'></div>".html_safe if user.is_admin
+  end
 end
