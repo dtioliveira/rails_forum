eeba6b758bf5c5569a929d2ff996ccb30874b721
Diego Oliveira <dti.oliveira@live.com>
2015-12-27 19:55:40 -0200

    Removendo modificações desnecessárias

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Bootsy::Engine => '/bootsy', as: 'bootsy'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
