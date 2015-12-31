d8a06761bfc421d92fa4f79aca6436f3c786e945
Diego Oliveira <dti.oliveira@live.com>
2015-12-31 10:56:43 -0200

    Teste de segurança com Brakeman

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :current_locale
