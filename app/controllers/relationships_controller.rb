7d4526a052b55756a6f934936cdd85eef1c79369
Diego Oliveira <dti.oliveira@live.com>
2015-12-30 23:51:16 -0200

    Listando solicitações de amizade para o usuário

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,11 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @user = current_user
+    @relationships = Relationship.where('adder_id = :id OR added_id = :id', id: current_user.id)
+  end
+
   def create
     @user = User.find(params[:relationship][:user_id])
     Relationship.create(adder_id: current_user.id, added_id: @user.id)
@@ -8,8 +13,8 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:user_id] || params[:relationship][:user_id])
-    Relationship.where(adder_id: current_user.id, added_id: @user.id).destroy_all
+    @user = User.find(params[:user_id])
+    Relationship.where(adder_id: [current_user.id, @user.id], added_id: [@user.id, current_user.id]).destroy_all
     @relationship = @user.has_relationship?(current_user).first
   end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,16 @@ class Relationship < ActiveRecord::Base
   scope :by_situation, -> (param) do
     where(situation: param)
   end
+
+  def adder_user
+    User.find(adder_id)
+  end
+
+  def added_user
+    User.find(added_id)
+  end
+
+  def displaying_for(user)
+    user == adder_user ? added_user : adder_user
+  end
 end
