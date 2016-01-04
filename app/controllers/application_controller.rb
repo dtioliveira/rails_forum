ef884545f2e4ded5f96e2a08d67fad7d98fe108d
Diego Oliveira <dti.oliveira@live.com>
2016-01-03 22:43:33 -0200

    Adicionando CSRF token ao form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,12 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   include Pundit
-  protect_from_forgery
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
